ed703157477d11771e7a102d0be6fd2ee98ba09f
unknown <TiTPain@yandex.ru>
2022-12-04 07:38:16 +0300

    Исправляет ошибки

--- index.js/index.js
+++ index.js/index.js
@@ -4,24 +4,24 @@ let popUpEdit = document.getElementById('popUp');
 let formElement = document.querySelector('#editForm');
 let nameInput = formElement.querySelector('#nameInput');
 let jobInput = formElement.querySelector('#jobInput');
+let profileName = document.querySelector('#profileName'); 
+let profileJob = document.querySelector('#profileJob');
 
 openEdit.addEventListener('click', function(evt) {
   evt.preventDefault();
-  popUpEdit.classList.add('active');
+  popUpEdit.classList.add('popup_opened');
 })
 
 closeEdit.addEventListener('click', function(evt) {
   evt.preventDefault();
-  popUpEdit.classList.remove('active');
+  popUpEdit.classList.remove('popup_opened');
 })
 
 function handleFormSubmit (evt) {
-    evt.preventDefault();
-    let profileName = document.querySelector('#profileName');
-    let profileJob = document.querySelector('#profileJob').textContent = jobInput.value;
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    popUpEdit.classList.remove('active');
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popUpEdit.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
